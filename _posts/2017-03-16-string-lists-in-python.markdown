5eb880cb3eb3fb652e8a127b4cd4f61377a79d6f
Alonso Indacochea <aindaco1@Alonsos-iMac.local>
2020-12-05 15:00:24 -0700

    initial commit of content

---
+++ _posts/2017-03-16-string-lists-in-python.markdown
@@ -0,0 +1,35 @@
+---
+layout: post
+title:  "Web application development - Comparing string lists in Python"
+display_title: "Comparing String Lists in Python"
+description: "Understanding how to compare string lists in Python for software and web application development."
+date:   2017-03-16 00:00:00 -0700
+categories: web-development
+image: /assets/img/python.png
+display_excerpt: "A few weeks ago, I ran into an interesting problem on a Python Flask project. Thankfully, Python is pretty slick."
+author: Jay Renteria
+author-title: Senior Developer
+---
+A few weeks ago, I ran into an interesting problem on a <a href="http://flask.pocoo.org/" target="_blank">Python Flask</a> project. An array of strings (in this case, email accounts) needed to be sorted - and if any of the strings contained specific words (like "test", for example), the associated user accounts needed to be modified slightly to indicate test user status.
+
+There are a number of different approaches you can take to accomplish this, but Python includes a really great function to help smooth this process out - and it's dead simple. 
+
+Let's start by making a test list: 
+
+{% highlight python %}
+test_list = ['example',  "example2", "rendrconsulting", "rendr", "jane@example3.com", "hermesdevelopment", "11online", "example4.com", "john@example5.com"]
+{% endhighlight %}
+
+Notice we have a good mix - email addresses, URLs, and just plain strings. We want to find any and all occurances of the specific strings in our email address data set and mark the user associated as a tester.
+
+{% highlight python %}
+test_mark = ''
+if any(word in test['email'] for word in test_list):
+	test_user = 'true'
+{% endhighlight %}
+
+It's as simple as that. What `any()` does is it checks if any of the words in our test data 'email' match any words defined in our `test_list`. If so, we set `test_user` to true. 
+
+For more reading on the `any()` (and its cousin, `all()`) function in Python, check out <a href="https://leemendelowitz.github.io/blog/any-all-in-python.html" target="_blank">this excellent write-up</a> that goes into more detail. 
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+function scroll() {
+	if(window.outerWidth > 800) {
+		var header = document.getElementById('site-header');
+		var height = header.clientHeight;
+		var body = document.getElementById('body');
+		if(document.getElementById('body').scrollTop > 120) {
+			header.className = 'site-header sticky';
+			if(body.className.indexOf('with-hero') == -1) {
+				body.style.marginTop = (height + 45) + "px";
+			}
+		} else {
+			header.className = 'site-header';
+			body.style.marginTop = "0px";
+		}
+	}
+}
+
+function bodyOnLoad() {
+	// if this is an IE browser, replace all svgs with pngs
+	if(detectIE()) {
+		var imgs = document.getElementsByTagName('img');
+		for (var i in imgs) {
+			if(typeof imgs[i].src !== 'undefined') {
+			    if (imgs[i].src.split('.')[1] == 'svg') {
+			        imgs[i].src = imgs[i].src.replace('.svg', '.png');
+			        // add a class name to use native png size
+			        imgs[i].className += imgs[i].className + " no-image-resize";
+			    }
+			}
+		}
+	}
+}
+
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  // IE 10
+  //ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+  
+  // IE 11
+  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  // other browser
+  return false;
+}
+
+function contactForm(event) {
+	event.preventDefault();
+	document.getElementById('send-button').value = "Sending...";
+	var name = event.target.name.value;
+	var email = event.target._replyto.value;
+	var body = event.target.body.value;
+
+	var xhttp = new XMLHttpRequest();
+  	xhttp.onreadystatechange = function() {
+    	if (this.readyState == 4 && this.status == 200) {
+      		document.getElementById('form-response').innerHTML = 'Email sent';
+      		document.getElementById('send-button').value = "Send";
+    	} else if (this.status >= 400) {
+    		document.getElementById('form-response').innerHTML = JSON.parse(this.response).error;
+    		document.getElementById('send-button').value = "Send";
+    		console.log(this.response);
+    	}
+  	};
+  	var jsonBody = {
+  		"name": name,
+  		"email": email,
+  		"body": body
+  	}
+	xhttp.open("POST", "https://formspree.io/s7v8x4i8f9n9r8r0@11online.slack.com", true);
+	xhttp.setRequestHeader("Content-type", "application/json");
+	xhttp.send(JSON.stringify(jsonBody));
+}
+
+/* InstantClick 3.1.0 | (C) 2014 Alexandre Dieulot | http://instantclick.io/license */
+var InstantClick=function(d,e){function w(a){var b=a.indexOf("#");return 0>b?a:a.substr(0,b)}function z(a){for(;a&&"A"!=a.nodeName;)a=a.parentNode;return a}function A(a){var b=e.protocol+"//"+e.host;if(!(b=a.target||a.hasAttribute("download")||0!=a.href.indexOf(b+"/")||-1<a.href.indexOf("#")&&w(a.href)==k)){if(J){a:{do{if(!a.hasAttribute)break;if(a.hasAttribute("data-no-instant"))break;if(a.hasAttribute("data-instant")){a=!0;break a}}while(a=a.parentNode);a=!1}a=!a}else a:{do{if(!a.hasAttribute)break;
+if(a.hasAttribute("data-instant"))break;if(a.hasAttribute("data-no-instant")){a=!0;break a}}while(a=a.parentNode);a=!1}b=a}return b?!1:!0}function t(a,b,c,g){for(var d=!1,e=0;e<B[a].length;e++)if("receive"==a){var f=B[a][e](b,c,g);f&&("body"in f&&(c=f.body),"title"in f&&(g=f.title),d=f)}else B[a][e](b,c,g);return d}function K(a,b,c,g){d.documentElement.replaceChild(b,d.body);if(c){history.pushState(null,null,c);b=c.indexOf("#");b=-1<b&&d.getElementById(c.substr(b+1));g=0;if(b)for(;b.offsetParent;)g+=
+b.offsetTop,b=b.offsetParent;scrollTo(0,g);k=w(c)}else scrollTo(0,g);d.title=S&&d.title==a?a+String.fromCharCode(160):a;L();C.done();t("change",!1);a=d.createEvent("HTMLEvents");a.initEvent("instantclick:newpage",!0,!0);dispatchEvent(a)}function M(a){G>+new Date-500||(a=z(a.target))&&A(a)&&x(a.href)}function N(a){G>+new Date-500||(a=z(a.target))&&A(a)&&(a.addEventListener("mouseout",T),H?(O=a.href,l=setTimeout(x,H)):x(a.href))}function U(a){G=+new Date;(a=z(a.target))&&A(a)&&(D?a.removeEventListener("mousedown",
+M):a.removeEventListener("mouseover",N),x(a.href))}function V(a){var b=z(a.target);!b||!A(b)||1<a.which||a.metaKey||a.ctrlKey||(a.preventDefault(),P(b.href))}function T(){l?(clearTimeout(l),l=!1):v&&!m&&(p.abort(),m=v=!1)}function W(){if(!(4>p.readyState)&&0!=p.status){q.ready=+new Date-q.start;if(p.getResponseHeader("Content-Type").match(/\/(x|ht|xht)ml/)){var a=d.implementation.createHTMLDocument("");a.documentElement.innerHTML=p.responseText.replace(/<noscript[\s\S]+<\/noscript>/gi,"");y=a.title;
+u=a.body;var b=t("receive",r,u,y);b&&("body"in b&&(u=b.body),"title"in b&&(y=b.title));b=w(r);h[b]={body:u,title:y,scrollY:b in h?h[b].scrollY:0};for(var a=a.head.children,b=0,c,g=a.length-1;0<=g;g--)if(c=a[g],c.hasAttribute("data-instant-track")){c=c.getAttribute("href")||c.getAttribute("src")||c.innerHTML;for(var e=E.length-1;0<=e;e--)E[e]==c&&b++}b!=E.length&&(F=!0)}else F=!0;m&&(m=!1,P(r))}}function L(a){d.body.addEventListener("touchstart",U,!0);D?d.body.addEventListener("mousedown",M,!0):d.body.addEventListener("mouseover",
+N,!0);d.body.addEventListener("click",V,!0);if(!a){a=d.body.getElementsByTagName("script");var b,c,g,e;i=0;for(j=a.length;i<j;i++)b=a[i],b.hasAttribute("data-no-instant")||(c=d.createElement("script"),b.src&&(c.src=b.src),b.innerHTML&&(c.innerHTML=b.innerHTML),g=b.parentNode,e=b.nextSibling,g.removeChild(b),g.insertBefore(c,e))}}function x(a){!D&&"display"in q&&100>+new Date-(q.start+q.display)||(l&&(clearTimeout(l),l=!1),a||(a=O),v&&(a==r||m))||(v=!0,m=!1,r=a,F=u=!1,q={start:+new Date},t("fetch"),
+p.open("GET",a),p.send())}function P(a){"display"in q||(q.display=+new Date-q.start);l||!v?l&&r&&r!=a?e.href=a:(x(a),C.start(0,!0),t("wait"),m=!0):m?e.href=a:F?e.href=r:u?(h[k].scrollY=pageYOffset,m=v=!1,K(y,u,r)):(C.start(0,!0),t("wait"),m=!0)}var I=navigator.userAgent,S=-1<I.indexOf(" CriOS/"),Q="createTouch"in d,k,O,l,G,h={},p,r=!1,y=!1,F=!1,u=!1,q={},v=!1,m=!1,E=[],J,D,H,B={fetch:[],receive:[],wait:[],change:[]},C=function(){function a(a,e){n=a;d.getElementById(f.id)&&d.body.removeChild(f);f.style.opacity=
+"1";d.getElementById(f.id)&&d.body.removeChild(f);g();e&&setTimeout(b,0);clearTimeout(l);l=setTimeout(c,500)}function b(){n=10;g()}function c(){n+=1+2*Math.random();98<=n?n=98:l=setTimeout(c,500);g()}function g(){h.style[k]="translate("+n+"%)";d.getElementById(f.id)||d.body.appendChild(f)}function e(){d.getElementById(f.id)?(clearTimeout(l),n=100,g(),f.style.opacity="0"):(a(100==n?0:n),setTimeout(e,0))}function m(){f.style.left=pageXOffset+"px";f.style.width=innerWidth+"px";f.style.top=pageYOffset+
+"px";var a="orientation"in window&&90==Math.abs(orientation);f.style[k]="scaleY("+innerWidth/screen[a?"height":"width"]*2+")"}var f,h,k,n,l;return{init:function(){f=d.createElement("div");f.id="instantclick";h=d.createElement("div");h.id="instantclick-bar";h.className="instantclick-bar";f.appendChild(h);var a=["Webkit","Moz","O"];k="transform";if(!(k in h.style))for(var b=0;3>b;b++)a[b]+"Transform"in h.style&&(k=a[b]+"Transform");var c="transition";if(!(c in h.style))for(b=0;3>b;b++)a[b]+"Transition"in
+h.style&&(c="-"+a[b].toLowerCase()+"-"+c);a=d.createElement("style");a.innerHTML="#instantclick{position:"+(Q?"absolute":"fixed")+";top:0;left:0;width:100%;pointer-events:none;z-index:2147483647;"+c+":opacity .25s .1s}.instantclick-bar{background:#29d;width:100%;margin-left:-100%;height:2px;"+c+":all .25s}";d.head.appendChild(a);Q&&(m(),addEventListener("resize",m),addEventListener("scroll",m))},start:a,done:e}}(),R="pushState"in history&&(!I.match("Android")||I.match("Chrome/"))&&"file:"!=e.protocol;
+return{supported:R,init:function(){if(!k)if(R){for(var a=arguments.length-1;0<=a;a--){var b=arguments[a];!0===b?J=!0:"mousedown"==b?D=!0:"number"==typeof b&&(H=b)}k=w(e.href);h[k]={body:d.body,title:d.title,scrollY:pageYOffset};for(var b=d.head.children,c,a=b.length-1;0<=a;a--)c=b[a],c.hasAttribute("data-instant-track")&&(c=c.getAttribute("href")||c.getAttribute("src")||c.innerHTML,E.push(c));p=new XMLHttpRequest;p.addEventListener("readystatechange",W);L(!0);C.init();t("change",!0);addEventListener("popstate",
+function(){var a=w(e.href);a!=k&&(a in h?(h[k].scrollY=pageYOffset,k=a,K(h[a].title,h[a].body,!1,h[a].scrollY)):e.href=e.href)})}else t("change",!0)},on:function(a,b){B[a].push(b)}}}(document,location);
+
